fa6bbe94f118cf63d0f2c0e5a048b391bdb2d850
dharmraj <dharmraj.prajapat@zemosolabs.com>
2017-07-12 15:29:15 +0530

    need to change

--- Error_handling/new_exception1.java
+++
@@ -1,8 +0,0 @@
-package Error_handling;
-
-/**
- * Created by zemoso on 1/7/17.
- */
-public class new_exception1 extends Exception {
-
-}
--- Error_handling/new_exception3.java
+++
@@ -1,7 +0,0 @@
-package Error_handling;
-
-/**
- * Created by zemoso on 1/7/17.
- */
-public class new_exception3 extends Exception {
-}
