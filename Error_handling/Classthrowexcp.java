b78e493f31fdc87117bf792ef030c528b8cd17a9
dharm033075 <dharmraj.prajapat@zemosolabs.com>
2017-07-24 17:05:11 +0530

    Update Classthrowexcp.java

--- Error_handling/Classthrowexcp.java
+++ Error_handling/Classthrowexcp.java
@@ -1,4 +1,7 @@
 package Error_handling;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -8,21 +11,21 @@ public class Classthrowexcp {
     public static void methodThrowError(int t) throws newException1, newException2, newException3 {
 
                   if(t==0){
-                      throw new newException1();
+                      throw new newException1("IO exception) ");
                   }else if(t==2){
                       throw new newException2();
                   }else {
-                      throw new newException3();
+                      throw new newException3("Some defined error");
                   }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws InputMismatchException  {
         System.out.println("Enter the Intger value");
         Scanner sc= new Scanner(System.in);
-        int t=sc.nextInt();
 
         try{
+            int t=sc.nextInt();
             methodThrowError(t);
-            }catch(newException1 | newException2 | newException3 e){
+            }catch(newException1 | newException2 | newException3 | InputMismatchException e) {
                e.printStackTrace(System.out);
            }finally{
                System.out.println("This have to be print coz its in finally");
