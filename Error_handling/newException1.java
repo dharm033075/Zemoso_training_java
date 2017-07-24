2b0584779f75fa2c16468476b49c91ea6256e29c
dharm033075 <dharmraj.prajapat@zemosolabs.com>
2017-07-24 17:05:40 +0530

    Update newException1.java

--- Error_handling/newException1.java
+++ Error_handling/newException1.java
@@ -1,8 +1,12 @@
 package Error_handling;
+import java.io.IOException;
+import java.util.InputMismatchException;
 
 /**
  * Created by zemoso on 1/7/17.
  */
-public class newException1 extends Exception {
-
+public class newException1 extends IOException{
+    public newException1(String message){
+        super(message);
+    }
 }
