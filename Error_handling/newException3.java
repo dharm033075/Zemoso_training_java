8ff7d42445ef13aa95e005f9a76467db5322ce3b
dharm033075 <dharmraj.prajapat@zemosolabs.com>
2017-07-24 17:06:16 +0530

    Update newException3.java

--- Error_handling/newException3.java
+++ Error_handling/newException3.java
@@ -1,7 +1,13 @@
 package Error_handling;
 
+import java.util.InputMismatchException;
+
 /**
  * Created by zemoso on 1/7/17.
  */
 public class newException3 extends Exception {
+    public newException3(String message){
+        super(message);
+    }
+
 }
